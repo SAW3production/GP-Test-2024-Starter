16f6b7bc3cdff3c6e0b2d12cfe9d315531fbd507
SAW3Production <105048323+SAW3production@users.noreply.github.com>
2024-05-02 13:47:09 +0100

    resize

--- Starter Code/spawnNodes.gd
+++ Starter Code/spawnNodes.gd
@@ -2,13 +2,13 @@ extends Sprite2D
 
 var icon = preload("res://icon.tscn")
 @onready var slider = $"../CanvasLayer/HSlider"
-const radius = 400
+const radius = 350
 @onready var num = $"../CanvasLayer/num"
 
 
 func _on_create_pressed():
 	var center = position
-	var theta = PI*2 / slider.max_value
+	var theta = PI*2 / slider.value
 	for i in range(1,slider.value+1):
 		var obj = icon.instantiate()
 		obj.position = Vector2(center.x + cos(theta * i) * radius, center.y - sin(theta *i)*radius)
