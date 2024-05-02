54feefb1b13818b361503e619ef9bf21cd18ab8d
SAW3Production <105048323+SAW3production@users.noreply.github.com>
2024-05-02 13:40:45 +0100

    hate trigenometry

--- Starter Code/spawnNodes.gd
+++ Starter Code/spawnNodes.gd
@@ -2,12 +2,19 @@ extends Sprite2D
 
 var icon = preload("res://icon.tscn")
 @onready var slider = $"../CanvasLayer/HSlider"
-
+const radius = 400
+@onready var num = $"../CanvasLayer/num"
 
 
 func _on_create_pressed():
-	for i in range(0,slider.value):
+	var center = position
+	var theta = PI*2 / slider.max_value
+	for i in range(1,slider.value+1):
 		var obj = icon.instantiate()
-		obj.position = Vector2(2,2)
-		add_child(obj)
+		obj.position = Vector2(center.x + cos(theta * i) * radius, center.y - sin(theta *i)*radius)
+		get_parent().add_child(obj)
 		
+
+
+func _on_h_slider_value_changed(value):
+	num.text = str(value)
